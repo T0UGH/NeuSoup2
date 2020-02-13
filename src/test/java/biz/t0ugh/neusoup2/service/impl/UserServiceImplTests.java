f20f5d33f8295f39b7f97f218207561b1061a7c6
t0ugh <1243384249@qq.com>
2020-02-13 19:42:22 +0800

    Feat:配置pageHelper插件

--- src/test/java/biz/t0ugh/neusoup2/service/impl/UserServiceImplTests.java
+++ src/test/java/biz/t0ugh/neusoup2/service/impl/UserServiceImplTests.java
@@ -1,14 +1,12 @@
-package biz.t0ugh.neusoup2;
+package biz.t0ugh.neusoup2.service.impl;
 
+import biz.t0ugh.neusoup2.Neusoup2ApplicationTests;
 import biz.t0ugh.neusoup2.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class Neusoup2ApplicationTests {
-
+public class UserServiceImplTests extends Neusoup2ApplicationTests {
     @Autowired
     UserService userService;
 
